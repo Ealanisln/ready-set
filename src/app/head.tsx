798a071f2871c73d0b5b7831c088d07b5bcdc733
Emmanuel Alanis <ealanis@192.168.1.4>
2024-09-23 10:16:05 -0600

    🔍️ Modified robots and header for SEO

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,6 +7,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="On-demand courier that specializes in delivery for all your needs. We are Food Safety, and HIPPA Certified. Our team can meet all your Bay Area delivery needs." />
       <link rel="icon" href="/images/favicon.ico" />
+      <meta name ="robots" content="index, follow"/>
     </>
   );
 }
