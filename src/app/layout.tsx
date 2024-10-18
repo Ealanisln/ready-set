61a8c19f94a0e9a64a1bd1570bc296caede78f1b
Emmanuel Alanis <ealanisln@me.com>
2024-10-18 08:49:07 -0700

    ✨ Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import ClientLayout from "@/components/Clients/ClientLayout";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
       </body>
+      <GoogleAnalytics gaId="G-PHGL28W4NP" />
     </html>
   );
 }
