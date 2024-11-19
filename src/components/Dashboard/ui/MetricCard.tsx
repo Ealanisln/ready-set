58d883218810b5f3a4cec7161813ab23c9eb40c2
Emmanuel Alanis <ealanisln@me.com>
2024-11-18 16:22:27 -0800

    🎨 Improved dashboard
    Added color and fixed cards

--- src/components/Dashboard/ui/MetricCard.tsx
+++ src/components/Dashboard/ui/MetricCard.tsx
@@ -1,29 +1,24 @@
+// MetricCard.tsx
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 import { LucideIcon } from 'lucide-react';
 
-// Types
 interface MetricCardProps {
     title: string;
     value: string | number;
     icon: LucideIcon;
     change: string;
-  }
-  
-// Metric Card Component
+}
+
 export const MetricCard: React.FC<MetricCardProps> = ({ title, value, icon: Icon, change }) => (
-    <Card>
+    <Card className="h-[140px] flex flex-col justify-center">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
         <Icon className="text-muted-foreground h-4 w-4" />
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">{value}</div>
-        {/* <p className="text-muted-foreground text-xs">{change}</p> */}
+        <p className="text-xs text-muted-foreground mt-1">{change}</p>
       </CardContent>
     </Card>
-  );
+);
