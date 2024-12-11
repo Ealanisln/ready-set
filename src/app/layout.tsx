e593da9e335693d2546793046c989bc132b0dd80
Emmanuel Alanis <ealanisln@me.com>
2024-12-10 16:22:25 -0800

    🐛 Fixed issue on consent banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,8 @@ import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { VercelToolbar } from "@vercel/toolbar/next";
-import MetricoolScript from "@/components/Metricool";
-import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
-import CookieConsentBanner from "@/components/Clients/banner";
+import ErrorBoundary from "@/components/ErrorBoundary/ErrorBoundary";
+import CookieConsentBanner from "@/components/Cookies/banner";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -29,17 +28,16 @@ export default function RootLayout({
     >
       <body className="overflow-x-hidden">
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
-          <ClientLayout>
-            {children}
-          </ClientLayout>
+          <ClientLayout>{children}</ClientLayout>
         </ErrorBoundary>
 
         {process.env.NODE_ENV === "development" && <VercelToolbar />}
         <Analytics />
-        <MetricoolScript />
         <SpeedInsights />
         <GoogleAnalytics gaId="G-PHGL28W4NP" />
-        <CookieConsentBanner />
+        <CookieConsentBanner
+          metricoolHash={process.env.NEXT_PUBLIC_METRICOOL_TRACKING_HASH || ""}
+        />
       </body>
     </html>
   );
