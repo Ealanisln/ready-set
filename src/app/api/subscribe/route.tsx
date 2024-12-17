0f1d1d135d4ac473b53dd2bd73331b0a800dd858
Emmanuel Alanis <ealanisln@me.com>
2024-12-16 17:30:22 -0800

    🚚 Updated api route to use brevo

--- src/app/api/subscribe/route.tsx
+++ src/app/api/subscribe/route.tsx
@@ -1,3 +1,4 @@
+// api/subscribe/route.ts
 import { NextResponse } from 'next/server';
 import axios from "axios";
 import { z } from "zod";
@@ -17,43 +18,46 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: "Please enter a valid email address" }, { status: 400 });
   }
 
-  // Retrieve Mailchimp credentials from environment variables
-  const API_KEY = process.env.MAILCHIMP_API_KEY;
-  const API_SERVER = process.env.MAILCHIMP_API_SERVER;
-  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID;
+  // Retrieve Brevo credentials from environment variables
+  const API_KEY = process.env.BREVO_API_KEY;
 
-  // Construct Mailchimp API request URL
-  const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`;
+  // Construct Brevo API request URL
+  const url = 'https://api.brevo.com/v3/contacts';
 
   // Prepare request data
   const data = {
-    email_address: emailValidation.data,
-    status: "subscribed",
+    email: emailValidation.data,
+    emailBlacklisted: false,
+    updateEnabled: true
   };
 
   // Set request headers
   const options = {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `api_key ${API_KEY}`,
+      "api-key": API_KEY,
     },
   };
 
-  // Send POST request to Mailchimp API
+  // Send POST request to Brevo API
   try {
     const response = await axios.post(url, data, options);
-    if (response.status == 200) {
-      return NextResponse.json({ message: "Awesome! You have successfully subscribed!" }, { status: 201 });
+    
+    // Brevo returns 201 for new contacts and 204 for updated contacts
+    if ([201, 204].includes(response.status)) {
+      return NextResponse.json({ 
+        message: "Awesome! You have successfully subscribed!" 
+      }, { status: 201 });
     }
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error(
         `${error.response?.status}`,
-        `${error.response?.data.title}`,
-        `${error.response?.data.detail}`,
+        `${error.response?.data.message}`,
       );
 
-      if (error.response?.data.title == "Member Exists") {
+      // Handle duplicate contact case
+      if (error.response?.data.message?.includes("Contact already exist")) {
         return NextResponse.json({
           error: "Uh oh, it looks like this email's already subscribed 🧐"
         }, { status: 400 });
