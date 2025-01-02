ced375fd54694ae175ed04a3bd1c5d8e4ebc3774
Fernando Cardenas <fsanchezcln@gmail.com>
2025-01-02 17:59:32 -0600

    :white_check_mark: Added link into apply now button to sign up

--- src/components/JoinTheTeam/VAModal.tsx
+++ src/components/JoinTheTeam/VAModal.tsx
@@ -105,7 +105,7 @@ export function VAModal() {
               <div className="font-medium mb-3 md:mb-4 text-sm md:text-base">
                 Ready to help businesses thrive while growing your career?
               </div>
-              <Link href="/your-application-page">
+              <Link href="/signup">
               <Button 
                 onClick={handleApplyClick}
                 className="bg-yellow-400 hover:bg-yellow-500 text-gray-800 w-full md:w-auto"
