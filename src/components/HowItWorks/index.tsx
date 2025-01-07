2a5f1bc44d825efe89ba33aabde7543643b13742
Fernando Cardenas <fsanchezcln@gmail.com>
2025-01-07 16:55:12 -0600

    Free guide component in process phase 2

--- src/components/HowItWorks/index.tsx
+++ src/components/HowItWorks/index.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import AppointmentDialog from "../VirtualAssistant/Appointment";
+import GuideForm from "../Resources/GuideForm";
 
 
 
@@ -85,6 +86,7 @@ const HowItWorks = () => {
             calendarUrl="https://calendar.google.com/calendar/appointments/AcZssZ1jHb5jHQLYMdGkYHDE1Joqi0ADTQ_QVVx1HcA=?gv=true&embedded=true"
           />
         </div>
+        <GuideForm />
         </div>
       </div>
   );
