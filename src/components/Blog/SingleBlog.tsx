51ef0295f828f3406747cf8962713095c0bf8847
Emmanuel Alanis <ealanisln@me.com>
2025-01-24 17:17:43 -0800

    💬 Added date to blog posts

--- src/components/Blog/SingleBlog.tsx
+++ src/components/Blog/SingleBlog.tsx
@@ -11,7 +11,6 @@ interface PostsProps {
 }
 
 const SingleBlog = ({ data, basePath }: PostsProps) => {
-  console.log('Post data:', data);
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 w-full max-w-7xl mx-auto">
       {data.map((post) => (
