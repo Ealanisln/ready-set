aa1f262c7201afca407e3c684927ed2e53082152
Fernando Cardenas <fsanchezcln@gmail.com>
2025-01-30 17:15:15 -0600

    :white_check_mark: Added routes to the new free guide pages

--- src/data/resources.tsx
+++ src/data/resources.tsx
@@ -1,5 +1,6 @@
 import EmailMetricsMatter from "@/components/Resources/EmailMetricsMatter";
 import EmailMarketingGuide from "@/components/Resources/EmailMarketing";
+import GuideChoosePartner from "@/components/Resources/GuideChoosePartner";
 
 export interface Resource {
   title: string;
@@ -15,4 +16,18 @@ export const resources: Resource[] = [
     imageUrl: "/images/resources/1.png",
     content: <EmailMetricsMatter />,
   },
+
+    {
+      title: "What Is Email Marketing",
+      description: "The Business Owner's Guide to Getting Started",
+      imageUrl: "/images/resources/2.png",
+      content: <EmailMarketingGuide />,
+    },
+
+    {
+      title: "The Complete Guide to Choosing the Right Delivery Partner",
+      description: "This comprehensive guide will help you navigate the complex process of selecting the right delivery partner for your business",
+      imageUrl: "/images/resources/3.png",
+      content: <GuideChoosePartner />,
+    },
 ];
