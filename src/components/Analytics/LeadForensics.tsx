af6aba91165ca7ca48d0f26d31db76777a7db4b0
Emmanuel Alanis <ealanisln@me.com>
2025-03-17 13:06:58 -0700

    🔨 Updated script

--- src/components/Analytics/LeadForensics.tsx
+++ src/components/Analytics/LeadForensics.tsx
@@ -3,10 +3,19 @@ import Script from 'next/script'
 
 export default function LeadForensics() {
   return (
-    <Script
-      src="https://www.perception-company.com/js/803213.js"
-      strategy="afterInteractive"
-      type="text/javascript"
-    />
+    <>
+      <Script
+        src="https://www.perception-company.com/js/803213.js"
+        strategy="afterInteractive"
+        type="text/javascript"
+      />
+      <noscript>
+        <img 
+          src="https://www.perception-company.com/803213.png" 
+          style={{ display: 'none' }} 
+          alt=""
+        />
+      </noscript>
+    </>
   )
 }
