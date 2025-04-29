3ab57b2494224625d6310625926da8b061cda72c
Emmanuel Alanis <ealanisln@me.com>
2025-04-28 17:49:08 -0700

    🐛 Added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 import { Montserrat } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import ClientLayout from "@/components/Clients/ClientLayout";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
@@ -23,6 +24,15 @@ export default function RootLayout({
       className={`!scroll-smooth ${montserrat.className}`}
       lang="en"
     >
+      <head>
+        {/* Umami Analytics Script (consent-free) */}
+        <Script
+          id="rs-tracker-app"
+          strategy="afterInteractive"
+          src="https://analytics-chi-wine.vercel.app/script.js"
+          data-website-id="41e73d42-53bf-4b9a-ae31-2e2d22a02a32"
+        />
+      </head>
       <body className="overflow-x-hidden">
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
           <ClientLayout>{children}</ClientLayout>
