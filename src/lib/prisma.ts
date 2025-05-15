a8554264fdb279b5cbc45e83a2e8860720b36c51
Emmanuel Alanis <ealanisln@me.com>
2025-05-15 15:51:26 -0700

    🐛 Fix prisma lib

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,24 @@
-import { prisma } from "@/utils/prismaDB";
+import { PrismaClient } from "@prisma/client";
 
-// This file is used to ensure Prisma client is initialized
-// before it's used in the app
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
+};
+
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
 export default prisma; 
