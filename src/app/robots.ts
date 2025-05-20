b1db295c8925cf6e9a206c3e1e02a650b9084fff
Emmanuel Alanis <ealanisln@me.com>
2025-05-20 14:23:26 -0700

    Simplify robots.ts to use basic MetadataRoute.Robots format

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,19 +1,17 @@
-import { NextResponse } from 'next/server';
+import { MetadataRoute } from 'next'
 
-export function GET() {
-  const robotsTxt = `User-agent: *
-Allow: /
-Disallow: /client/deliveries/
-Disallow: /driver/deliveries/
-Disallow: /reset-password/
-Disallow: /studio/
-
-Sitemap: https://readysetllc.com/sitemap.xml
-`;
-
-  return new NextResponse(robotsTxt, {
-    headers: {
-      'Content-Type': 'text/plain',
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/client/deliveries/',
+        '/driver/deliveries/',
+        '/reset-password/',
+        '/studio/',
+      ],
     },
-  });
+    sitemap: 'https://readysetllc.com/sitemap.xml',
+  }
 }
