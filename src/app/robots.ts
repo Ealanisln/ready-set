9a91e43bccf776b3697a7952f656cec9b5e78b4c
Emmanuel Alanis <ealanisln@me.com>
2025-05-20 14:00:56 -0700

    Fix build errors: Update robots.ts structure and remove skipMetadataRoutes from next.config.js

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,18 +2,16 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: [
-          '/client/deliveries/',
-          '/driver/deliveries/',
-          '/reset-password/',
-          '/studio/',
-        ],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/client/deliveries/',
+        '/driver/deliveries/',
+        '/reset-password/',
+        '/studio/',
+      ],
+    },
     sitemap: 'https://readysetllc.com/sitemap.xml',
   };
 } 
