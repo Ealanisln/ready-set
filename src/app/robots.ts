e3b2beab75ab095195652dd5b673c3c37d09a4fc
Emmanuel Alanis <ealanisln@me.com>
2025-05-20 15:01:20 -0700

    🐛 fix: Update robots.ts to use dynamic base URL and correct format

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,17 +1,30 @@
 import { MetadataRoute } from 'next'
 
+/**
+ * Generate robots.txt content
+ * Using 'use cache' to ensure this can be statically generated
+ */
 export default function robots(): MetadataRoute.Robots {
+  'use cache'
+  
+  // Use BASE_URL env var if defined, otherwise fallback to the Coolify deploy URL
+  const baseUrl = process.env.BASE_URL || 
+    process.env.NEXT_PUBLIC_BASE_URL || 
+    'http://jsk8c8cwg4w84co88ckgkg48.91.99.110.92.sslip.io';
+  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/client/deliveries/',
-        '/driver/deliveries/',
-        '/reset-password/',
-        '/studio/',
-      ],
-    },
-    sitemap: 'https://readysetllc.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/client/deliveries/',
+          '/driver/deliveries/',
+          '/reset-password/',
+          '/studio/',
+        ],
+      }
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
